32de88617e7f87669d285a50b4e81b6d1597300e
Priyanka Dhar <priyankadhar2002@gmail.com>
2024-01-25 20:21:04 +0530

    Add files via upload

---
+++ Day18.py
@@ -0,0 +1,18 @@
+import sys
+
+class Solution:
+    def __init__(self):
+        self.queue = []
+        self.stack = []
+
+    def pushCharacter(self, ch):
+        self.stack.append(ch)
+
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+
+    def popCharacter(self):
+        return self.stack.pop()
+
+    def dequeueCharacter(self):
+        return self.queue.pop(0)
